b9d93da7e79d8d3e5c5c0ab7e3534fb55692dbe3
ABhatt14 <7r6pwzyw6p@privaterelay.appleid.com>
2026-09-13 05:28:41 +0530

    Update contact heading

---
+++ contact-override.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const heading = document.getElementById("contactHeading");
+  if (heading) heading.innerHTML = "Got a story?<br><em>Let's cut to it.</em>";
+});
